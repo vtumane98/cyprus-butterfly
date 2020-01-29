9de595a24b7dcb5164ae8a23fea8f9445d41476d
Nikita Komissarov <vtumane98@gmail.com>
2020-01-29 19:46:10 +0300

    main-section

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,46 @@
 document.addEventListener("DOMContentLoaded", function () {
 
+    $('.main-slider').owlCarousel({
+        items:1,
+        loop:true,
+        nav:true,
+        dots:true,
+        smartSpeed:650,
+    })
+
+    $('.card-slider').owlCarousel({
+        items:4,
+        nav:true,
+        dots:true,
+        smartSpeed:250,
+        slideBy: 4,
+        autoWidth: true,
+        mouseDrag: false,
+    })
+
+    $('.goods-slider').owlCarousel({
+        items:4,
+        smartSpeed:250,
+        slideBy: 4,
+        
+    })
+
+    $('.photo-slider').owlCarousel({
+        items:1,
+        loop:true,
+        nav:true,
+        dots:true,
+        smartSpeed:650,
+    })
+
+    $(".transport-type__item").click(function(){
+        $(".transport-type__item").removeClass("active");
+        $(this).addClass("active")
+    })
+
+    $(".rent-type__item").click(function(){
+        $(".rent-type__item").removeClass("active");
+        $(this).addClass("active")
+    })
+
 })
