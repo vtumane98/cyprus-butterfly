8d5f9dabeec09b39a79857bd29ee625cc70d53a1
Nikita Komissarov <vtumane98@gmail.com>
2020-01-31 08:49:41 +0300

    <=480px

--- app/js/main.js
+++ app/js/main.js
@@ -1,46 +1,115 @@
-document.addEventListener("DOMContentLoaded", function () {
+$(document).ready(function () {
 
-    $('.main-slider').owlCarousel({
-        items:1,
-        loop:true,
-        nav:true,
-        dots:true,
-        smartSpeed:650,
-    })
+    var mainSlider = new Swiper('.main-slider', {
+        speed: 650,
+        pagination: {
+            el: '.main-slider .slider-pagination',
+            clickable: true,
+            bulletActiveClass: "active",
+        },
+        navigation: {
+            nextEl: '.slider-button-next',
+            prevEl: '.slider-button-prev',
+        },
+        autoplay: {
+            delay: 4000,
+        },
+    });
 
-    $('.card-slider').owlCarousel({
-        items:4,
-        nav:true,
-        dots:true,
-        smartSpeed:250,
-        slideBy: 4,
-        autoWidth: true,
-        mouseDrag: false,
-    })
+    var cardSlider = new Swiper('.card-slider', {
+        speed: 450,
+        spaceBetween: 16,
+        pagination: {
+            el: '.card-slider .slider-pagination',
+            clickable: true,
+            bulletActiveClass: "active",
+        },
+        navigation: {
+            nextEl: '.card-slider .slider-button-next',
+            prevEl: '.card-slider .slider-button-prev',
+        },
+        breakpoints: {
+            1440: {
+              slidesPerView: 4,
+              slidesPerGroup: 4,
+            },
+            1140: {
+                slidesPerView: 3,
+                slidesPerGroup: 3,
+            },
+            970: {
+                slidesPerView: 3,
+                slidesPerGroup: 3,
+                spaceBetween: 12,
+            },
+            0: {
+                slidesPerView: "auto",
+                slidesPerGroup: 1,
+                spaceBetween: 18,
+                freeMode: true,
+            },
+        },
+    });
 
-    $('.goods-slider').owlCarousel({
-        items:4,
-        smartSpeed:250,
-        slideBy: 4,
-    })
+    var goodsSlider = new Swiper('.goods-slider', {
+        speed: 450,
+        spaceBetween: 14,
+        simulateTouch: false,
+        breakpoints: {
+            1440: {
+              slidesPerView: 4,
+            },
+            1140: {
+                slidesPerView: 3,
+            },
+            970: {
+                slidesPerView: 3,
+            },
+            0: {
+                slidesPerView: "auto",
+                slidesPerGroup: 1,
+                spaceBetween: 16,
+                freeMode: true,
+                simulateTouch: true,
+            },
+        },
+    });
 
-    $('.photo-slider').owlCarousel({
-        items:1,
-        loop:true,
-        nav:true,
-        dots:true,
-        smartSpeed:650,
-    })
+    var photoSlider = new Swiper('.photo-slider', {
+        speed: 450,
+        slidesPerView: 1,
+        pagination: {
+            el: '.photo-slider .slider-pagination',
+            clickable: true,
+            bulletActiveClass: "active",
+        },
+        navigation: {
+            nextEl: '.photo-slider .slider-button-next',
+            prevEl: '.photo-slider .slider-button-prev',
+        },
+    });
 
-    $('.location-slider').owlCarousel({
-        items:2,
-        smartSpeed:250,
-        slideBy: 2,
-        margin: 8,
-        nav:true,
-        dots:true,
-    })
+    var locationSlider = new Swiper('.location-slider', {
+        speed: 450,
+        slidesPerView: 2,
+        slidesPerGroup: 2,
+        spaceBetween: 30,
+        pagination: {
+            el: '.location-slider .slider-pagination',
+            clickable: true,
+            bulletActiveClass: "active",
+        },
+        navigation: {
+            nextEl: '.location-slider .slider-button-next',
+            prevEl: '.location-slider .slider-button-prev',
+        },
+    });
 
+    $(".transport-type__tab").slice(1).removeClass("active");
+
+    $(".rent-type__tab").slice(1).removeClass("active");
+
+    
     $(".transport-type__item").on("click", function() {
         $(this).addClass("active").siblings().removeClass("active");
         $(".transport-type__tab").removeClass("active").eq($(this).index()).addClass("active");
@@ -51,4 +120,10 @@ document.addEventListener("DOMContentLoaded", function () {
         $(".rent-type__tab").removeClass("active").eq($(this).index()).addClass("active");
     })
 
+    $(".burger-menu").click(function(){
+        $(".header-nav").addClass("active")
+    })
+    $(".header-nav__row").click(function(){
+        $(".header-nav").removeClass("active")
+    })
 })
